43d08a4d51c6a06609858a51efb328af035a515e
Christopher M Lyver <lyverc@captaincommando.ccs.neu.edu>
2014-10-03 17:51:16 -0400

    Remove erroneous wav file.  Use the wav file Clinger provides (they work, others don't).  Use numpy and wavfile.read to get some initial data from the wav file.

--- wavData.py
+++ wavData.py
@@ -1,11 +1,22 @@
 __author__ = 'christopherlyver'
-import numpy
+import numpy as np
 from scipy.io import wavfile
 import pdb
 
-pdb.set_trace()
-sample, data = wavfile.read("alrighty.wav")
+#pdb.set_trace()
 
-print sample
-print data[:5]
+# Wav audio files in our directory, taken from Clinger's audio samples.
+file1 = 'wayfaring.wav'
+file2 = 'excerpt0.wav'
+
+# I've run into some trouble if I don't use Clinger's supplied wav files.
+# ^Unsure why
+
+# Read() returns a tuple (sample rate, data read from array)
+sample_rate, stereo_data = wavfile.read(file2)
+
+print sample_rate
+# Data is a list of tuple lists, representing stero sound (left and right)
+# Print out the first ten frequency samples
+print stereo_data[:10]
 
