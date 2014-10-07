28510822b9ca3c69c1b25136cfd6dd181f44e09e
Wilfred Julius Hsie <wjhsie@lastresort.ccs.neu.edu>
2014-10-07 18:35:16 -0400

    uploading visualizer.py

---
+++ visualizer.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+from scipy.io.wavfile import read
+import sys, wave
+import numpy as np
+from scipy import fftpack
+
+# take wav file from cmd line
+new_wav = sys.argv[1]
+print new_wav
+
+# open wav file
+wav_data = read(new_wav)
+
+# open wav file into wave_read object
+wave_read_data = wave.open(new_wav)
+sample_rate, audio = wav_data
+#print sample_rate
+print audio
+
+fft_data = np.fft.rfft(audio)
+print fft_data
+
+seconds = wave_read_data.getnframes() / sample_rate
+print seconds
+
+all_freqs = np.fft.fftfreq(len(fft_data))
+
+idx = np.argmax(np.abs(fft_data)**2)
+print idx
+freqs_in_hertz = []
+for f in fft_data:
+	freqs_in_hertz.append(abs(f*sample_rate))
+print freqs_in_hertz[:1000]
+
+#  sample rate is 44100 samples per second
+#  so lets see the whole .wav file by taking the number of 
+#  seconds and multiplying that by 44100, to get the total
+#  frames we should plot
+plt.plot(audio[0:wave_read_data.getnframes()])
+plt.ylabel("amplitude")
+plt.xlabel("time")
+plt.title(new_wav)
+plt.show()
+
+plt.plot(freqs_in_hertz)
+plt.show()
