7de948ccfe466db7b659a5b3edbfd1ccf5875d02
Christopher M Lyver <lyverc@asterix.ccs.neu.edu>
2014-10-07 13:11:36 -0400

    Scratch the surface of getting frequencies out of wavfile.read()

--- Test.py
+++ Test.py
@@ -6,14 +6,14 @@ from compare import Compare
 test = WavData('audio/excerpt0.wav')
 
 test.get_frequencies()
-print test.frequencies
+print test.frequencies[:10]
 
-print "************************************************************************"
 # call fft on the freqs
 test.get_fft()
-for freq in test.frequencies:
-	print freq
+print test.frequencies[:10]
 
+"""
 #A basic test to see that the same song is equal to itself
 test2 = Compare('audio/excerpt0.wav', 'audio/excerpt0.wav', 50)
 test2.naive_compare()
+"""
--- wavData.py
+++ wavData.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy.fft import fft
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
-
+import pylab as pl
 
 #pdb.set_trace()
 
@@ -20,15 +20,21 @@ class WavData():
         """
         Use wavfile.read() to get sample rate and stereo data from the wav file
         """
-        sample_rate, stereo_data = wavfile.read(self.file)
+        sample_rate, data = wavfile.read(self.file)
         self.sample_rate = sample_rate
-        self.frequencies = stereo_data
+        self.frequencies = data
 	
-	# Try to plot the incoming data
-	plt.figure(1)
-	plt.title('Signal Wave...')
-	plt.plot(stereo_data)
 
+
+	p = 20*np.log10(np.abs(np.fft.rfft(data[:2048, 0])))
+	f = np.linspace(0, sample_rate/2.0, len(p))
+	pl.plot(f, p)
+	pl.xlabel("Frequency(Hz)")
+	pl.ylabel("Power(dB)")
+	pl.show()
+ 	
+	
+	
     def make_mono(self):
 	"""
 	Take stereo data and make it into mono data
@@ -46,5 +52,9 @@ class WavData():
 
     def get_fft(self):
 	self.frequencies = fft(self.frequencies)
+	data = self.frequencies
+
+
+	
 
 
