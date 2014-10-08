c695af2a1927c86ae7c1c856117229499d0e631b
wilhsie <wjhsie@knuckleheads.ccs.neu.edu>
2014-10-08 19:51:37 -0400

    divided Hz by 100000

--- visualizer.py
+++ visualizer.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from scipy.io.wavfile import read
+from scipy.io import wavfile
 import sys, wave
 import numpy as np
 from scipy import fftpack
@@ -9,15 +9,12 @@ new_wav = sys.argv[1]
 print new_wav
 
 # open wav file
-wav_data = read(new_wav)
+sample_rate, wav_data = wavfile.read(new_wav)
 
 # open wav file into wave_read object
 wave_read_data = wave.open(new_wav)
-sample_rate, audio = wav_data
-#print sample_rate
-print audio
 
-fft_data = np.fft.rfft(audio)
+fft_data = np.fft.rfft(wav_data)
 print fft_data
 
 seconds = wave_read_data.getnframes() / sample_rate
@@ -29,14 +26,14 @@ idx = np.argmax(np.abs(fft_data)**2)
 print idx
 freqs_in_hertz = []
 for f in fft_data:
-	freqs_in_hertz.append(abs(f*sample_rate))
+	freqs_in_hertz.append(abs(f*sample_rate)/100000)
 print freqs_in_hertz[:1000]
 
 #  sample rate is 44100 samples per second
 #  so lets see the whole .wav file by taking the number of 
 #  seconds and multiplying that by 44100, to get the total
 #  frames we should plot
-plt.plot(audio[0:wave_read_data.getnframes()])
+plt.plot(wav_data[0:wave_read_data.getnframes()])
 plt.ylabel("amplitude")
 plt.xlabel("time")
 plt.title(new_wav)
