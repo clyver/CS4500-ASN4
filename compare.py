4afe5a8e8a73e9c52b3c067bc585a5a33f6d8646
Christopher M Lyver <lyverc@batsugun.ccs.neu.edu>
2014-11-19 18:12:53 -0500

    Updata our error threshold

--- compare.py
+++ compare.py
@@ -1,6 +1,7 @@
 __author__ = 'christopherlyver'
 from wavData import WavData
-
+import pdb
+import numpy as np
 
 class Compare():
     """
@@ -15,13 +16,31 @@ class Compare():
 	self.f1 =  WavData(f1)
 	self.f1.get_duration()
 	self.f1.make_mono()
-	self.f1.make_min_max_samples()
-			
+	pdb.set_trace()
+	self.f1.chunk(4096)
+	wav_to_fft = np.array([])
+	for chunk in self.f1.frequencies:
+		fft_chunk = self.f1.get_rfft(chunk)
+		wav_to_fft.append(fft_chunk)
+	pdb.set_trace()
+	self.f1.frequencies = wav_to_fft
+	
 	self.f2 =  WavData(f2)
 	self.f2.get_duration()
 	self.f2.make_mono()
-	self.f2.make_min_max_samples()
+	self.f2.chunk(4096)
+	wav_to_fft = []
+	for chunk in self.f2.frequencies:
+                fft_chunk = self.f2.get_rfft(chunk)
+                wav_to_fft.append(fft_chunk)
 
+	self.f2.frequencies = wav_to_fft
+
+
+	i = 0
+	while i < len(self.f1.frequencies):
+		print "f1: ", self.f1.frequencies[0][i], " f2: ", self.f2.frequencies[0][i]
+		i += 1
 	self.limit = alloted_diff
 	self.deviation_limit = equal_threshold
 
@@ -29,10 +48,18 @@ class Compare():
 	"""
 	Return True if the two numbers are within a threshold of each other
 	"""
-	if abs(freq1 - freq2) <= self.deviation_limit:
+	
+	dist = np.linalg.norm(freq1-freq2)
+	print "dist", dist	
+	if dist <= self.deviation_limit:
 		return True
 	else:
 		return False
+
+	#if abs(freq1 - freq2) <= self.deviation_limit:
+	#	return True
+	#else:
+	#		return False
 	
 
     def compare(self):
@@ -40,7 +67,7 @@ class Compare():
 	Do a simple comare of two files' mono frequencies
 	We say the two songs are equal if they have less then 'limit' diffences
 	"""
-	
+ 	pdb.set_trace()	
 	# Do an intitial test to make sure the files are the same duration
 	if self.f1.duration != self.f2.duration:
 		#print "These songs are not the same duration, and thus differ"
@@ -64,10 +91,10 @@ class Compare():
 		ind1 = 0
 		# Go through the samples and check for diff. If diff += 1 errors
 		for i in f1_noise:
-			for j in i:
+			for j in f2_noise:
 				# FOR TESTING PURPOSES
 				#print "comparing {t1} with {t2}".format(t1=j, t2=f2_noise[ind0][ind1])
-				if not self.equal(j, f2_noise[ind0][ind1]):
+				if not self.equal(i, j):
 		         		errors += 1
 				if(ind1 == 1):
 					ind1 -= 1
